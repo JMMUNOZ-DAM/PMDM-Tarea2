4bf146ad12fb5c77be806f25254807074bfc44e0
JMMUNOZ-DAM <juanmamunoz1990@gmail.com>
2024-12-08 12:07:19 +0100

    First commit

--- app/src/main/java/dam/pmdm/pmdmtarea02/ui/home/HomeFragment.java
+++ app/src/main/java/dam/pmdm/pmdmtarea02/ui/home/HomeFragment.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
